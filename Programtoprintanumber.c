5f0af0ce92f43906e9de375fb8b3ceffbab14a13
ShynoSAlexander <86480554+ShynoSAlexander@users.noreply.github.com>
2022-01-21 18:28:58 +0530

    Update Programtoprintanumber.c

--- Programtoprintanumber.c
+++ Programtoprintanumber.c
@@ -4,7 +4,7 @@
 int main()
 {
     int x=100;  //x is avariable
-    printf("The number is : %d",x);
+    printf("The number is : %d",x);/*Here %d is a format specifier where %d specifies the type of variable as decimal.In output console you will see the value of x(i.e,100) instead of %d*/
 
     return 0;
 }
