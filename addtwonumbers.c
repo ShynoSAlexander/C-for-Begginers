439ed7aca164e7c06d1df1e3cf675efb36b9b5a2
ShynoSAlexander <86480554+ShynoSAlexander@users.noreply.github.com>
2022-01-21 18:45:53 +0530

    Create addtwonumbers.c

---
+++ addtwonumbers.c
@@ -0,0 +1,12 @@
+//PROGRAM TO ADD TWO NUMBERS
+#include <stdio.h>
+int main()
+{
+    int x,y,z;  //Declaring x ,y,z variables
+    x=10;
+    y=20;
+    z=x+y;//adds the value of x and y and stores it in z.
+    printf("The value of z is: %d",z);//prints the value stored in z
+
+    return 0;
+}
